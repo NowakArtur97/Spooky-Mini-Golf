9c5fc07b59364de567b89bf1616102c784ceb6f0
NowakArtur97 <nowart97@gmail.com>
2024-02-01 17:43:58 +0100

    Create script to handle hand's animations

---
+++ Assets/Hazards/Hand/HandAnimationHandler.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using UnityEngine;
+
+public class HandAnimationHandler : MonoBehaviour
+{
+    [Header("Attributes")]
+    [SerializeField] private float _hidenTime = 3.0f;
+    [SerializeField] private float _idleTime = 3.0f;
+
+    private Animator _myAnimator;
+
+    private void Awake() => _myAnimator = GetComponent<Animator>();
+
+    private void Start() => StartCoroutine(Wait(_hidenTime, "MoveUp"));
+
+    public void TriggerHiddenState() => StartCoroutine(Wait(_hidenTime, "MoveUp"));
+
+    public void TriggerIdleState() => StartCoroutine(Wait(_idleTime, "MoveDown"));
+
+    private IEnumerator Wait(float waitTime, string triggerName)
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        _myAnimator.SetTrigger(triggerName);
+    }
+}
